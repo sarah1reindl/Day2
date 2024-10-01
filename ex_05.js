2069d06edbd0fcfb0277e6e10d31f6c0252d78c1
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-01 14:35:00 +0200

    ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,28 +1,17 @@
-function getBasketContent() {
-    let fruits = [];
-
-    fruits.push("strawberry");
-    fruits.push("strawberry");
-    fruits.push("strawberry");
-    fruits.push("apple");
-    fruits.push("apple");
-    fruits.push("lime");
-    fruits.push("lime");
-    fruits.push("peach");
-    fruits.push("pear");
-    fruits.push("pear");
-
-    let totaldefruits = fruits.length;
-
-    console.log("Total: " + totaldefruits + " fruits"); 
-    
-    return fruits; 
+let straws = 3
+let apples = 2
+let limes = 2
+let peach = 1
+let pears = 2
+ 
+let array = ["straws", "straws", "straws", "apples", "apples", "limes", "limes", "peach", "pears", "pears"]
+ 
+function getBasketContent() { 
+return array
+ 
 }
-
-
-getBasketContent(); 
-
-
+ 
+console.log(array.length)
 
 // DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
 if (typeof getBasketContent === "function") {
